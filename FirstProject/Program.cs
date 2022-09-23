1935c4480e76ac866d698cab5d030a11ad28b7ac
Shortisc <shortisc@gmail.com>
2022-09-24 01:15:31 +0300

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        //You can read what are Methods :)
+        Console.WriteLine(SayHi("Plamen and Svetlyo"));
+    }
+
+    public static string SayHi(string name)
+    {
+        return $"Hello {name}";
+    }
+}
